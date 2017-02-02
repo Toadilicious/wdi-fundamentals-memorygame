191dbc35c4350b223baaa1b580c0a894f3d047a0
Todd Gerdy <toddgerdy@Todds-iMac.local>
2017-02-02 16:19:00 -0500

    I have added unit 10 assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,8 +5,22 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo === cardFour) {
+/*if (cardTwo === cardFour) {
 	alert ("You found a match!");
 } else {
 	alert ("Sorry, try again.");
+}*/
+function createCards(x) {
+	// find how many elements have 'game-board as Id'
+	var findGameBoard = document.getElementById('game-board');
+
+	// create an HTML card for each card with class of 'card'
+	for(var i = 0; i < x; i++) {
+	// add the elements	
+		var newCard = document.createElement('div');
+	// give them the class of 'card'
+		newCard.className = 'card';
+	// 
+		document.querySelector("#game-board").appendChild(newCard);
+	}
 }
